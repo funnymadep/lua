6629688c4b628621373ca012efb728c134bdda11
Spectrum.何清林 <Qinglin.He@spectrum-sys.cn>
2025-06-18 10:02:20 +0800

    hello lua

---
+++ lua_learn/hello.lua
@@ -0,0 +1,24 @@
+print("hello world")
+print("你好 世界")
+
+-- print("hello world") 单行注释
+
+--[[ 多行注释
+print("hello world")
+print("hello world")
+print("hello world")
+--]]
+
+--[[
+变量语法与C一致 区分大小写 首个无数字
+--]]
+
+-- nil 是空 lua访问空不会报错 只会得到nil
+print(b)
+
+c = 10
+print(c)
+
+--[[
+	lua hello.lua
+]]
